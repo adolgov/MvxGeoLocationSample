b82eea2fe38b25068f8d05ea31d86e6dd0c58aea
Tomasz Cielecki <tomasz@ostebaronen.dk>
2014-06-24 15:02:02 +0200

    Using nuget instead of submodule.

---
+++ PlayServicesLocation.Droid/Bootstrap/LocationPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace PlayServicesLocation.Droid.Bootstrap
+{
+    public class LocationPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Location.PluginLoader>
+    {
+    }
+}
