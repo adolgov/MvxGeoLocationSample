a3bb503da156dc0a6200daa64d7a8cd966f9b0aa
Tomasz Cielecki <tomasz@ostebaronen.dk>
2014-06-24 15:03:58 +0200

    Clean up after nugets.

--- MvxGeoLocationSample/Bootstrap/LocationBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace MvxGeoLocationSample.Bootstrap
-{
-    public class LocationBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Location.PluginLoader>
-    {
-    }
-}
--- MvxGeoLocationSample/Bootstrap/LocationPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace MvxGeoLocationSample.Bootstrap
-{
-    public class LocationPluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Location.PluginLoader>
-    {
-    }
-}
